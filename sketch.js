7cc06947e04606737db95e392bc6ae68052dc412
Abdul Imran <Hitimran@gmail.com>
2019-02-10 02:00:03 +0100

    dvd challaenge

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let x;
 let y;
 let xSpeed;
 let ySpeed;
-
+let r,g,b;
 
 function setup() {
   createCanvas(800, 600);
@@ -10,6 +10,13 @@ function setup() {
   y=300;
   xSpeed=10;
   ySpeed=10;
+  pickcolor();
+}
+
+function pickcolor() {
+  r=random(100,256);
+  g=random(100,256);
+  b=random(100,256);
 }
 
 let dvd;
@@ -20,15 +27,18 @@ dvd= loadImage("dvd_logo.png");}
 function draw() {
   background(0);
  // rect(x,y,80,60);
+  tint(r,g,b);
   image(dvd,x,y);
   x=x+xSpeed;
   y=y+ySpeed;
   
   if(x +80== width || x==0)
   {xSpeed=-xSpeed;
+   pickcolor();
   }
   
   if(y +60== height || y==0)
   {ySpeed=-ySpeed;
+   pickcolor();
   }
 }
